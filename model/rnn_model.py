58c908cd276b5455170f9d4a4e4c2b48a8bce0ac
Pulkit <pgoel3379@gmail.com>
2024-09-01 09:52:12 +0530

    add the trained model

--- model/rnn_model.py
+++ model/rnn_model.py
@@ -3,31 +3,43 @@ import tensorflow as tf
 
 class RNNModel(tf.keras.Model): 
 
-    def __init__(self, vocabulary_size = 10_000): 
+    def __init__(self, vocabulary_size = 1000): 
         super().__init__()
         self.vocab_size = vocabulary_size
 
         self.textEncoder = tf.keras.layers.TextVectorization(max_tokens=self.vocab_size) 
 
-        self.embedding = tf.keras.layers.Embedding(input_dim = self.vocab_size + 1 , output_dim = 32, mask_zero = True) 
-        self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = 32))
-        self.dense1 = tf.keras.layers.Dense(units = 32)
+        self.embedding = tf.keras.layers.Embedding(input_dim = self.vocab_size + 1 , output_dim = 64, mask_zero = True) 
+        self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = 64))
+        self.dense1 = tf.keras.layers.Dense(units = 32, activation = 'relu')
         self.dense2 = tf.keras.layers.Dense(units = 1) 
-
-        self.activation = tf.keras.layers.ReLU() 
         
+    def build(self, input_shape): 
 
+        self.textEncoder.build(input_shape) 
+        input_shape = self.textEncoder.compute_output_shape(input_shape) 
+
+        self.embedding.build(input_shape) 
+        input_shape = self.embedding.compute_output_shape(input_shape) 
+
+        self.lstm.build(input_shape) 
+        input_shape = self.lstm.compute_output_shape(input_shape) 
+
+        self.dense1.build(input_shape) 
+        input_shape = self.dense1.compute_output_shape(input_shape) 
+
+        self.dense2.build(input_shape) 
+        input_shape = self.dense2.compute_output_shape(input_shape) 
+
+        self.built = True 
 
     def call(self, x): 
         x = self.textEncoder(x)
 
         x = self.lstm(self.embedding(x)) 
 
-        output = self.dense2(self.activation(self.dense1(x))) 
-
-        return self.activation(output) 
-
-
+        output = self.dense2(self.dense1(x)) 
+        return output 
 
     def train_encoder(self, train_data: tf.data.Dataset):
 
--- model/train.py
+++ model/train.py
@@ -1,20 +1,34 @@
 import tensorflow as tf 
 from data_utils import load_data
 from rnn_model import RNNModel 
+import matplotlib.pyplot as plt 
 
 BATCH_SIZE = 256
+VOCAB_SIZE = 500
+EPOCHS = 100
 
-train_set, test_set = load_data("./data/processed_data", 0.5) 
+train_set, test_set = load_data("./data/processed_data", 0.7) 
 
-train_set = train_set.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+train_set = train_set.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE) 
 test_set = test_set.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE) 
 
-model = RNNModel() 
+model = RNNModel(VOCAB_SIZE) 
 
-# the forward pass is working, complete the model class with fit method and loss function and run on the train_set. 
+model.compile(optimizer = tf.keras.optimizers.Adam(0.001), loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics = "accuracy") 
 
-model.compile(optimizer = tf.keras.optimizers.Adam(1e-4), loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics = "accuracy") 
-
-print("training the model...")
 model.train_encoder(train_set)
-model.fit(train_set, epochs = 10, validation_data=test_set, validation_steps= 10)
+history = model.fit(train_set, epochs = EPOCHS)
+
+model.save_weights("./model/trained_model/")
+
+plt.plot(range(1, EPOCHS + 1), history.history["loss"], marker = "o")
+plt.xlabel("Epochs")
+plt.ylabel("Training Loss")
+plt.savefig("./model/plots/training_loss.png") 
+plt.close() 
+
+plt.plot(range(1, EPOCHS + 1), history.history["accuracy"], marker = "o", color = "red")
+plt.xlabel("Epochs") 
+plt.ylabel("Training accuracy") 
+plt.savefig("./model/plots/training_accuracy.png")
+plt.close() 

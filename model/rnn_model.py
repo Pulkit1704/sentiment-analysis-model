1eb59cb55ad94dc3d1c75de86bfeea6fd4279e93
Pulkit <pgoel3379@gmail.com>
2025-01-30 20:07:44 +0530

    change the model architecture and retrain the model

--- model/rnn_model.py
+++ model/rnn_model.py
@@ -3,45 +3,57 @@ import tensorflow as tf
 
 class RNNModel(tf.keras.Model): 
 
-    def __init__(self, vocabulary_size = 1000): 
-        super().__init__()
+    def __init__(self, vocabulary_size: int = 1000, **kwargs): 
+        super().__init__(**kwargs)
         self.vocab_size = vocabulary_size
 
-        self.textEncoder = tf.keras.layers.TextVectorization(max_tokens=self.vocab_size) 
-
         self.embedding = tf.keras.layers.Embedding(input_dim = self.vocab_size + 1 , output_dim = 64, mask_zero = True) 
-        self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = 64))
+        self.lstm = tf.keras.layers.LSTM(units = 64) 
+        self.bidirectional = tf.keras.layers.Bidirectional(self.lstm)
         self.dense1 = tf.keras.layers.Dense(units = 32, activation = 'relu')
         self.dense2 = tf.keras.layers.Dense(units = 1) 
-        
-    def build(self, input_shape): 
+        self.dropout = tf.keras.layers.Dropout(0.8) 
 
-        self.textEncoder.build(input_shape) 
-        input_shape = self.textEncoder.compute_output_shape(input_shape) 
-
-        self.embedding.build(input_shape) 
-        input_shape = self.embedding.compute_output_shape(input_shape) 
-
-        self.lstm.build(input_shape) 
-        input_shape = self.lstm.compute_output_shape(input_shape) 
-
-        self.dense1.build(input_shape) 
-        input_shape = self.dense1.compute_output_shape(input_shape) 
-
-        self.dense2.build(input_shape) 
-        input_shape = self.dense2.compute_output_shape(input_shape) 
-
-        self.built = True 
-
-    def call(self, x): 
-        x = self.textEncoder(x)
-
-        x = self.lstm(self.embedding(x)) 
-
-        output = self.dense2(self.dense1(x)) 
-        return output 
-
-    def train_encoder(self, train_data: tf.data.Dataset):
-
-        self.textEncoder.adapt(train_data.map(lambda text, labels: text)) 
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "vocab_size": self.vocab_size,
+            "embedding_config": self.embedding.get_config(),
+            "lstm_config": self.lstm.get_config(),
+            "dense1_config": self.dense1.get_config(),
+            "dense2_config": self.dense2.get_config(),
+            "dropout_config": self.dropout.get_config()
+        })
+        return config
     
+    @classmethod
+    def from_config(cls, config: dict):
+        # Extract layer configurations
+        vocab_size = config.pop("vocab_size") 
+        embedding_config = config.pop("embedding_config")
+        lstm_config = config.pop("lstm_config")
+        dense1_config = config.pop("dense1_config")
+        dense2_config = config.pop("dense2_config")
+        dropout_config = config.pop("dropout_config") 
+
+        # Create a new instance of the model
+        instance = cls(**config)
+
+        # Rebuild the layers with the saved configurations
+        instance.vocab_size = vocab_size
+        instance.embedding = tf.keras.layers.Embedding.from_config(embedding_config)
+        instance.lstm = tf.keras.layers.LSTM.from_config(lstm_config)
+        instance.bidirectional = tf.keras.layers.Bidirectional(instance.lstm) 
+        instance.dense1 = tf.keras.layers.Dense.from_config(dense1_config)
+        instance.dense2 = tf.keras.layers.Dense.from_config(dense2_config)
+        instance.dropout = tf.keras.layers.Dropout.from_config(dropout_config) 
+
+        return instance
+
+    def call(self, x: tf.Tensor): 
+
+        x = self.bidirectional(self.embedding(x))
+
+        output = self.dense2(self.dropout(self.dense1(x))) 
+
+        return output 
--- model/train.py
+++ model/train.py
@@ -3,9 +3,21 @@ from data_utils import load_data
 from rnn_model import RNNModel 
 import matplotlib.pyplot as plt 
 
-BATCH_SIZE = 256
-VOCAB_SIZE = 500
-EPOCHS = 100
+BATCH_SIZE = 128
+VOCAB_SIZE = 300
+EPOCHS = 50
+
+def save_history(history, filepath): 
+
+    for key, value in history.items():
+
+        plt.plot(range(1, len(value)+1), value, marker = "o") 
+        plt.ylabel(f"{key}")
+        plt.xlabel("Epochs")
+        plt.grid(True) 
+        plt.savefig(f"{filepath}/{key}.png") 
+        plt.close()
+
 
 train_set, test_set = load_data("./data/processed_data", 0.7) 
 
@@ -14,21 +26,14 @@ test_set = test_set.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
 model = RNNModel(VOCAB_SIZE) 
 
-model.compile(optimizer = tf.keras.optimizers.Adam(0.001), loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics = "accuracy") 
+model.compile(optimizer = tf.keras.optimizers.Adam(0.01), loss = tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics = ["accuracy"]) 
 
-model.train_encoder(train_set)
-history = model.fit(train_set, epochs = EPOCHS)
+callback = tf.keras.callbacks.EarlyStopping(monitor = "val_accuracy", patience = 5, restore_best_weights= True)
 
-model.save_weights("./model/trained_model/")
+history = model.fit(train_set, epochs = EPOCHS, validation_data = test_set, callbacks = [callback])
 
-plt.plot(range(1, EPOCHS + 1), history.history["loss"], marker = "o")
-plt.xlabel("Epochs")
-plt.ylabel("Training Loss")
-plt.savefig("./model/plots/training_loss.png") 
-plt.close() 
+model.summary()
 
-plt.plot(range(1, EPOCHS + 1), history.history["accuracy"], marker = "o", color = "red")
-plt.xlabel("Epochs") 
-plt.ylabel("Training accuracy") 
-plt.savefig("./model/plots/training_accuracy.png")
-plt.close() 
+model.save("./trained_model/model.keras") 
+
+save_history(history.history, "./trained_model/training_plots/")
